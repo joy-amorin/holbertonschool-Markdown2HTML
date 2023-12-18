78324529c74395817b18d9d57f01a6e3d9fbebd6
Johana Amorin <amorinkaren2021@gmail.com>
2023-12-18 13:01:57 -0800

    documentation

--- markdown2html.py
+++ markdown2html.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" convert markdown to html """
+""" Write a script markdown2html.py that takes an argument 2 strings:
+First argument is the name of the Markdown file
+Second argument is the output file name """
 
 import sys
 import markdown2
