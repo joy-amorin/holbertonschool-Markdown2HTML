aa2ecdeda7da8c45de8a2924dbf1a4f060231f7d
Johana Amorin <amorinkaren2021@gmail.com>
2023-12-18 12:24:00 -0800

    documentation

--- markdown2html.py
+++ markdown2html.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-""" 
-convert markdown to html 
-"""
+""" convert markdown to html """
 
 import sys
 import markdown2
 
 def convert_markdown_to_html(input_file, output_file):
-    """ 
-    function to convert markdown to html 
-    """
+    """ function to convert markdown to html"""
 
     try:
         with open(input_file, 'r', encoding='utf-8') as md_file:
