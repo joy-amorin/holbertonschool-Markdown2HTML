87347b71e14d00dcdc1fce1efd61017d23460ec1
Johana Amorin <amorinkaren2021@gmail.com>
2023-12-19 12:06:40 -0800

    remove split to list

--- markdown2html.py
+++ markdown2html.py
@@ -20,7 +20,6 @@ def convert_markdown_to_html(md_file, html_file):
 
     for line in md_content:
         if line.startswith("#"):
-            # Cerrar la lista si está abierta antes del encabezado
             if in_list:
                 html_content += "</ul>\n"
                 in_list = False
@@ -35,18 +34,16 @@ def convert_markdown_to_html(md_file, html_file):
             if not in_list:
                 html_content += "<ul>\n"
                 in_list = True
-            list_items = line.lstrip("-").strip().split()
-            for item in list_items:
-                html_content += f"<li>{item}</li>\n"
+            list_item = line.lstrip("-").strip()
+            html_content += f"<li>{list_item}</li>\n"
 
         else:
-            # Cerrar la lista si no es una línea de encabezado o lista
+
             if in_list:
                 html_content += "</ul>\n"
                 in_list = False
             html_content += line
 
-    # Cerrar la lista si está abierta al final del archivo
     if in_list:
         html_content += "</ul>\n"
 
