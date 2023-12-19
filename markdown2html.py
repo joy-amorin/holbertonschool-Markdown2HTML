dd6b93ebd677b2b46ac7dc94633b2c7bee29e076
Johana Amorin <amorinkaren2021@gmail.com>
2023-12-19 08:40:21 -0800

    remove code and write again without library

--- markdown2html.py
+++ markdown2html.py
@@ -4,31 +4,34 @@ First argument is the name of the Markdown file
 Second argument is the output file name """
 
 import sys
-import markdown2
+import os
 
-def convert_markdown_to_html(input_file, output_file):
-    """ function to convert markdown to html """
 
-    try:
-        with open(input_file, 'r', encoding='utf-8') as md_file:
-            md_content = md_file.read()
-            html_content = markdown2.markdown(md_content)
+def convert_markdown_to_html(md_file, html_file):
+    """ function to convert Markdown file to HTML."""
 
-            with open(output_file, 'w', encoding='utf-8') as html_file:
-                html_file.write(html_content)
-
-    except FileNotFoundError:
-        sys.stderr.write(f"Missing {input_file}\n")
+    if not os.path.isfile(md_file):
+        print(f"Missing {md_file}", file=sys.stderr)
         sys.exit(1)
 
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
-        sys.exit(1)
+    with open(md_file, 'r') as markdown_file:
+        md_content = markdown_file.read()
 
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
+    html_content = f"<html><body>{md_content}</body></html>"
 
-    convert_markdown_to_html(input_file, output_file)
+    with open(html_file, 'w') as html_file:
+        html_file.write(html_content)
 
     sys.exit(0)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html")
+        sys.exit(1)
+
+    md_file = sys.argv[1]
+    html_file = sys.argv[2]
+
+    convert_markdown_to_html(md_file, html_file)
